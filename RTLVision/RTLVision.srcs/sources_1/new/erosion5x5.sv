f561ba2268612cbc1f7f3026bf6dd776bd2cd9a9
Osman Yasal <osmyasal@gmail.com>
2026-05-12 17:06:49 +0300

    ..

---
+++ RTLVision/RTLVision.srcs/sources_1/new/erosion5x5.sv
@@ -0,0 +1,90 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/12/2026 04:52:39 PM
+// Design Name: 
+// Module Name: erosion5x5
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module erosion5x5 #(parameter IMG_WIDTH = 1280)(
+        input logic clk,
+        input logic rst,
+        input logic in_valid,
+        input logic [7:0] in_pixel,
+        input logic [24:0] in_kernel,
+        output logic [7:0] out_pixel,
+        output logic out_valid
+    );
+
+    logic       pipeline_full;
+    logic       pipeline_empty;
+    logic       window_valid;
+    logic [7:0] r_data [0:24];
+
+    
+    fifo_pipeline5 #(
+        .DATA_WIDTH(8),
+        .DEPTH(IMG_WIDTH)
+    ) u_window (
+        .clk(clk),
+        .rst(rst),
+        .w_data(in_pixel),
+        .w_en(in_valid),
+        .full(pipeline_full),
+        .r_en(1'b1),                
+        .r_data(r_data),
+        .out_data_valid(window_valid)
+    );
+    
+    // Combinational logic signals for the minimum tree
+    logic [7:0] current_and;
+    
+    // -----------------------------------------------------------------
+    // Combinational AND Tree with Dynamic Kernel Masking
+    // -----------------------------------------------------------------
+    always_comb begin
+        // Initialize to 1 (True). A logical AND chain must start at 1.
+        current_and = 8'b1111_1111; 
+        
+        for (int i = 0; i < 25; i++) begin
+            // If the kernel bit is active, AND the current state with the pixel.
+            // If the pixel is 0, it pulls the whole chain down to 0 permanently.
+            if (in_kernel[i] == 1'b1) begin
+                current_and = current_and & r_data[i]; 
+            end
+        end
+    end
+
+    // -----------------------------------------------------------------
+    // Pipeline Output Register
+    // -----------------------------------------------------------------
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            out_valid <= 0;
+            out_pixel <= 0;
+        end
+        else if (window_valid) begin
+            // Register the result of our combinational AND tree
+            out_pixel <= current_and;
+            out_valid <= 1'b1;
+        end
+        else begin
+            out_valid <= 0;
+            out_pixel <= 0;
+        end
+    end
+endmodule
